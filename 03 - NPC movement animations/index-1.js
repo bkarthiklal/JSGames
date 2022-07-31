d06c21ee2821abfaf2e26703d669da762b218ec9
Karthik <bkarthiklal@gmail.com>
2022-07-31 22:51:12 +0530

    03 - NPC movement animation -  Added wave motion to Sprite 2

--- 03 - NPC movement animations/index-1.js
+++ 03 - NPC movement animations/index-1.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-let numberOfEnemies = 15;
+let numberOfEnemies = 25;
 const enemiesArray = []
 
 const enemyImage = new Image();
@@ -21,11 +21,16 @@ class Enemy {
     this.y = Math.random() * (canvas.height- this.height);
 
     this.frame = 0;
-    this.flapSpeed = Math.floor(Math.random() * 3 + 1) ;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = 0;
+    // this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve  = Math.random() * 7;
   }
   update() {
     this.x -= this.speed;
-    // this.y += Math.random() * 5- 2.5;
+    this.y += this.curve * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
     if (this.x + this.width < 0) {
       this.x = canvas.width;
     }
