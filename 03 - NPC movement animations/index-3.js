21ff35cc9843731cf9abe1c78beaa04ea2f0770b
Karthik <bkarthiklal@gmail.com>
2022-07-31 23:25:54 +0530

    03 - NPC movement animation  - Sprite 4 Patter 1

--- 03 - NPC movement animations/index-3.js
+++ 03 - NPC movement animations/index-3.js
@@ -11,33 +11,31 @@ let gameFrame = 0;
 class Enemy {
   constructor(path) { 
     this.image = new Image();
-    this.image.src = './images/enemy3.png';
+    this.image.src = './images/enemy4.png';
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
-    this.y = Math.random() * (canvas.height- this.height);
-
+    this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * (canvas.width - this.width);
+    this.newY = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = 0; // Where in path the anim char appear at first 
-    // this.angle = Math.random() * 2;
-    this.angleSpeed = Math.random() * 1.5 + 0.5;
-    this.curve = Math.random() * 200 + 50;
-    
-    /**
-     * Play with (Math.PI / 'x'), x  to change the angle of the enemy
-     */
+    this.interval = Math.floor(Math.random() * 200 + 50)
   }
   update() {
-    this.x = canvas.width/2 * Math.sin(this.angle * Math.PI / 90) +
-      (canvas.width / 2 - this.width/2)
-    this.y = canvas.height/2 * Math.cos(this.angle * Math.PI / 360) +
-      (canvas.height / 2 - this.height/2)
-    // this.y += this.curve * Math.sin(this.angle);
-    this.angle += this.angleSpeed;
+    if (gameFrame % this.interval === 0) { 
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx / 70
+    this.y -= dy / 70
+    
     if (this.x + this.width < 0) {
       this.x = canvas.width;
     }
