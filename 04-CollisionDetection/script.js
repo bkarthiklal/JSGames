32863300677bf45c86008def33d0999773c5fdff
Karthik <bkarthiklal@gmail.com>
2022-08-01 00:39:47 +0530

    04 Collision Detection - Collision animation and sound effects

--- 04-CollisionDetection/script.js
+++ 04-CollisionDetection/script.js
@@ -7,38 +7,74 @@ const explosions = [];
 
 class Explosion {
   constructor(x, y) { 
-    this.x = x;
-    this.y = y;
     this.spriteWidth = 200;
     this.spriteHeight = 179;
-    this.width = this.spriteWidth / 2;
-    this.height = this.spriteHeight / 2;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = './images/boom.png';
     this.frame = 0;
-
+    this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = './sounds/boom.wav';
   }
   update() {
-    this.frame++
+    if (this.frame === 0) {
+      this.sound.play()
+    }
+    this.timer++;
+    if (this.timer % 10 === 0) { 
+      this.frame++
+    }
   }
   draw() { 
     /* ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh); */
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
     ctx.drawImage(
       this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width / 2,
+      0 - this.height / 2,
       this.width,
       this.height
     );
+    ctx.restore();
   }
+}
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(
+    new Explosion(positionX, positionY)
+  );
 }
 
 window.addEventListener('click', function (e) {
-  ctx.fillStyle = '#fff';
-  // ctx.fillRect(e.x - canvasPosition.left, e.y - canvasPosition.top, 50, 50); // Draws at the click position
-  ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50); // Draws from center of click position
+  createAnimation(e)
 });
+// window.addEventListener('mousemove', function (e) {
+//   createAnimation(e)
+// });
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let index = 0; index < explosions.length; index++) {
+    explosions[index].update()
+    explosions[index].draw()
+    if(explosions[index].frame > 5) {
+      explosions.splice(index, 1)
+      index--
+    }
+  }
+  requestAnimationFrame(animate);
+};
+
+animate();
