7b49737478164ca7627b041956ecab0899c66f7d
Karthik <bkarthiklal@gmail.com>
2022-08-23 00:40:14 +0530

    Base Worm movement animation completed

--- 06-EnemyVariety/script.js
+++ 06-EnemyVariety/script.js
@@ -5,37 +5,88 @@ window.addEventListener('load', function () {
   canvas.height = 500;
 
   class Game {
-    constructor() {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
+      this.enemyInterval = 1000;
+      this.enemyTimer = 0;
     }
-    update() {
-
+    update(deltaTime) {
+      /** Delta time is to ensure same speed of rendering on
+       * machines of different speed and refresh rates
+       */
+      if (this.enemyTimer > this.enemyInterval) {
+        /** Check for enemies outside canvas and delete them */
+        this.enemies = this.enemies.filter(x => !x.markedForDeletion)
+        
+        this.#addNewEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach(enemy => enemy.update(deltaTime));
     }
     draw() {
-
+      this.enemies.forEach(enemy => enemy.draw());
     }
     /** Private method */
     #addNewEnemy() {
-
+      this.enemies.push(new Worm(this));
     }
   }
 
   class Enemy {
-    constructor() {
-
+    constructor(game) {
+      this.game = game;
+      this.markedForDeletion = false;
     }
-    update() {
-
+    update(deltaTime) {
+      this.x -= this.vx * deltaTime;
+      if (this.x < 0 - this.width) {
+        this.markedForDeletion = true;
+      }
     }
     draw() {
+      this.game.ctx.drawImage(
+        this.image, /** Image src */
+        0, 
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x, this.y, this.width, this.height)
+    }
+  } 
 
+  class Worm extends Enemy {
+    constructor(game) {
+      /** Super used to call parent constructor
+       * call super before using this object 
+       */
+      super(game);
+      this.spriteWidth = 229;
+      this.spriteHeight = 171;
+      this.width = this.spriteWidth/2;
+      this.height = this.spriteHeight/2;
+      this.x = this.game.width;
+      this.y = this.game.height * Math.random();
+      this.image = worm;
+      this.vx = Math.random() * 0.1 + 0.1;
     }
   }
 
-  function animate() {
+
+  const game = new Game(ctx, canvas.width, canvas.height);
+  let lastTime = 1;
+  function animate(timeStamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    game.update(deltaTime);
+    game.draw();
     /** Some code */
     requestAnimationFrame(animate)
-
-  }
+  };
+  animate(0);
 })
