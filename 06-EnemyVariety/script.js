a9393300fbd83d65616031074e7532823a52fcdd
Karthik <bkarthiklal@gmail.com>
2022-08-23 00:38:59 +0530

    Running the script after page and asset load (css and images)

--- 06-EnemyVariety/script.js
+++ 06-EnemyVariety/script.js
@@ -1,39 +1,41 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 500;
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 500;
+  canvas.height = 500;
 
-class Game {
-  constructor() {
-    this.enemies = [];
+  class Game {
+    constructor() {
+      this.enemies = [];
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+    /** Private method */
+    #addNewEnemy() {
+
+    }
   }
-  update() {
+
+  class Enemy {
+    constructor() {
+
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    /** Some code */
+    requestAnimationFrame(animate)
 
   }
-  draw() {
-
-  }
-  /** Private method */
-  #addNewEnemy() {
-
-  }
-}
-
-class Enemy {
-  constructor() {
-
-  }
-  update() {
-
-  }
-  draw() {
-
-  }
-}
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  /** Some code */
-  requestAnimationFrame(animate)
-
-}
+})
